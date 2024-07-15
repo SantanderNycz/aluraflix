29721b50fd449d6579aaf019a57b38ae502946a1
Leo Santander Nycz <121593322+SantanderNycz@users.noreply.github.com>
2024-07-15 11:02:52 +0100

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 const URL_BACKEND_TOP = window.location.hostname.includes("localhost")
-  ? "http://localhost:8080"
-  : "https://myaluraflix.herokuapp.com";
+  ? "http://localhost:3000"
+  : "https://my-json-server.typicode.com/typicode/demo";
 
 export default {
   URL_BACKEND_TOP,
