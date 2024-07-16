4db223ae0b614d5b834eae841fc305716352b45a
Leo Santander Nycz <121593322+SantanderNycz@users.noreply.github.com>
2024-07-16 10:05:54 +0100

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 const URL_BACKEND_TOP = window.location.hostname.includes("localhost")
   ? "http://localhost:3000"
-  : "https://aluraflix-topaz.vercel.app/";
+  : "https://my-json-server.typicode.com/santandernycz/aluraflix-api";
 
 export default {
   URL_BACKEND_TOP,
